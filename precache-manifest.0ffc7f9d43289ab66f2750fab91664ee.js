28facfd75b352f82935bcdfa333feba30431cb77
nic <nic@yexiaodongdeMacBook-Pro.local>
2020-04-12 00:13:06 +0800

    feat: add builded html

---
+++ precache-manifest.0ffc7f9d43289ab66f2750fab91664ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e479ac208915858421558e14ffd51684",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6b168b1df1ddac51fc8",
+    "url": "/static/css/2.a6d27ea7.chunk.css"
+  },
+  {
+    "revision": "74d61d0261c664c7e6c3",
+    "url": "/static/css/main.44e46ad5.chunk.css"
+  },
+  {
+    "revision": "d6b168b1df1ddac51fc8",
+    "url": "/static/js/2.8ddf96e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8ddf96e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d61d0261c664c7e6c3",
+    "url": "/static/js/main.36066bb6.chunk.js"
+  },
+  {
+    "revision": "c7c4e9ca645c4d9c0596",
+    "url": "/static/js/runtime-main.a2ff9661.js"
+  }
+]);
